0651599c8d7a0628e60dce59f5399731608829e1
Mauricio Santaella Ochoa <mao8a87@gmail.com>
2019-12-06 01:35:32 -0600

    cleaner family object algorithm

--- src/store/index.js
+++ src/store/index.js
@@ -24,127 +24,81 @@ export default new Vuex.Store({
     storePeopleData: (state, val) => {
       state.people = val
     },
-    storeFamilyTreeData: (state, val) => {
-      state.familyTree = createTreeObj(val)
+    storeFamilyTreeData: (state, objectP) => {
+      let flyCount = 1
+      let usedObject = {}
+      let treObject = {
+        'familia': {
+          'couple': []
+        }
+      }
+      const lowestRow = objectP[Object.keys(objectP)[0]].row
+      // loop en todos los elementos como llegan de firebase
+      for (let key in objectP) {
+        // si es = a lowest row
+        if (objectP[key].row === lowestRow) {
+          agregarTreeObject(treObject, '', key, true)
+          usedObject[key] = 'familia'
+        } else {
+          const nextFlyCount = flyCount + 1
+          // si encuentra mother en usados
+          if (objectP[key].conections.mother in usedObject) {
+            const mother = objectP[key].conections.mother
+            agregarTreeObject(treObject, usedObject[mother], key, `familia${nextFlyCount}`, true)
+            // agrego key a usados
+            usedObject[key] = usedObject[mother] + `.relatives.familia${nextFlyCount}`
+          
+          // si encuentra father in usados
+          } else if (objectP[key].conections.father in usedObject) {
+            let father = objectP[key].conections.father
+            agregarTreeObject(treObject, usedObject[father], key, `familia${nextFlyCount}`, true)
+            // agrego key a usados
+            usedObject[key] = usedObject[father] + `.relatives.familia${nextFlyCount}`
+          }
+        }
+      }
 
-      function createTreeObj (objectP) {
-        let flyCount = 1
-        let usedObject = {}
-        let treObject = {
-          'familia': {
-            'couple': []
-          }
+      for (let key in objectP) {
+        // estoy buscando los que no se han usado (no pertenecen a la familia directa)
+        if (key in usedObject) continue
+        const spouse = objectP[key].conections.spouse
+        // ahora buscamos al spaouse en los usados 
+        if (spouse in usedObject) {
+          agregarTreeObject(treObject, usedObject[spouse], key)
+          usedObject[key] = usedObject[spouse]
         }
-  
-        const lowestRow = objectP[Object.keys(objectP)[0]].row
-  
-        // loop en todos los elementos como llegan de firebase
-        for (let key in objectP) {
-          // si es = a lowest row
-          if (objectP[key].row === lowestRow) {
-            // console.log(objectP[key].name + " lowest ROW");
-  
-            agregarTreeObject(treObject, '', key, true)
-  
-            usedObject[key] = []
-            usedObject[key].push('familia')
-          } else {
-            // si encuentra mother en usados
-            if (findUsed(objectP[key].conections.mother)) {
-              let mother = objectP[key].conections.mother
-              // console.log(objectP[key].name + " MOTHER: " + objectP[mother].name);
-  
-              let newPath = usedObject[mother][0] + '.relatives.familia' + (flyCount + 1)
-              agregarTreeObject(treObject, usedObject[mother][0], key, 'familia' + (flyCount + 1), true)
-              // agrego key a usados
-              usedObject[key] = []
-              usedObject[key].push(newPath)
-            
-            // si encuentra father in usados
-            } else if (findUsed(objectP[key].conections.father)) {
-              let father = objectP[key].conections.father
-              // console.log(objectP[key].name + " FATHER: " + objectP[father].name);
-  
-              let newPath = usedObject[father][0] + '.relatives.familia' + (flyCount + 1)
-              agregarTreeObject(treObject, usedObject[father][0], key, 'familia' + (flyCount + 1), true)
-  
-              // agrego key a usados
-              usedObject[key] = []
-              usedObject[key].push(newPath)
+      }
+
+      state.familyTree = treObject
+
+      function agregarTreeObject (obj, path, key, familyId, relation = false) {
+        // separa el string en un array
+        const parts = path.split('.')
+        // si el array tiene un solo valor
+        if (parts.length === 1) {
+          const lastpath = parts[0]
+          if (lastpath === '') {
+            obj.familia.couple.push(key)
+          } else if (relation) { // si relation es true(realtive)            
+            if (!('relatives' in obj[lastpath])) {
+              // creo una propiedad 'relatives' en el objeto provisional
+              obj[lastpath].relatives = {}
             }
+            // dentro de 'relatives' creo una propiedad 'family#' basada en el counter
+            obj[lastpath].relatives[familyId] = {}
+            // dentro de 'family#' creo una propiedad 'couple' como array
+            obj[lastpath].relatives[familyId].couple = []
+            // y le agrego a 'couple' el key
+            obj[lastpath].relatives[familyId].couple.push(key)
+            // ahora le sumo 1 a el caunter para el nombre de la familia
+            flyCount = flyCount + 1
+          } else if (!relation) { // si relation es false(couple)
+            // al path le agrego el key al 'couple' al que pertenece. la pareja ya debe estar en el objProvicional
+            obj[lastpath].couple.push(key)
           }
-        }
-  
-        for (let key in objectP) {
-          // console.log("***PERSONA C: " + objectP[key].name);
-          if (!usedObject[key]) {
-            // si encuentra spouse in usados
-            if (findUsed(objectP[key].conections.spouse)) {
-              let spouse = objectP[key].conections.spouse
-              // console.log(objectP[key].name + " SI encontre SPOUSE: " + objectP[spouse].name);
-  
-              agregarTreeObject(treObject, usedObject[spouse][0], key)
-              usedObject[key] = []
-              usedObject[key].push(usedObject[spouse][0])
-            }
-          }
-        }
-  
-        return treObject
-  
-        function findUsed (key) {
-          let usados = Object.keys(usedObject)
-          let resultado = false
-          for (var i = 0; i < usados.length; i++) {
-            if (usados[i] === key) {
-              resultado = true
-            }
-          }
-          return resultado
-        }
-  
-        function agregarTreeObject (obj, path, key, familyId = false, relation = false) {
-          // separa el string en un array
-          // console.log("path: " + path);
-          let parts = path.split('.')
-          // console.log(parts);
-  
-          // si el array tiene un solo valor
-          if (parts.length === 1) {
-            if (parts[0] === '') {
-              obj.familia.couple.push(key)
-              return true
-            
-            // si relation es true(realtive)
-            } else if (relation) {
-              let familyObj = parts[0]
-              // console.log(obj[familyObj]);
-  
-              if (!obj[familyObj].relatives) {
-                // creo una propiedad 'relatives' en el objeto provisional
-                obj[familyObj].relatives = {}
-              }
-              // dentro de 'relatives' creo una propiedad 'family#' basada en el counter
-              obj[familyObj].relatives[familyId] = {}
-              // dentro de 'family#' creo una propiedad 'couple' como array
-              obj[familyObj].relatives[familyId].couple = []
-              // y le agrego a 'couple' el key
-              obj[familyObj].relatives[familyId].couple.push(key)
-              // ahora le sumo 1 a el caunter para el nombre de la familia
-              flyCount = flyCount + 1
-              // retorno para que pare la funcion
-              return true
-            
-            // si relation es false(couple)
-            } else {
-              // al path le agrego el key al 'couple' al que pertenece. la pareja ya debe estar en el objProvicional
-              obj[parts[0]].couple.push(key)
-              // retorno para que pare la funcion
-              return true
-            }
-          }
+        } else {
           // si 'path' tiene mas de un item se reinicia la misma funcion con un item menos pero con el objeto dentro de un nivel mas
-          return agregarTreeObject(obj[parts[0]], parts.slice(1).join('.'), key, familyId, relation)
+          agregarTreeObject(obj[parts[0]], parts.slice(1).join('.'), key, familyId, relation)
         }
       }
     },
